da27cccd17cfc9467a940169870e449549660ea9
Anik927 <mirzaanik927@gmail.com>
2026-07-11 02:00:03 +0600

    task 1

---
+++ Task_1/FirstMVCApp/Controllers/StudentController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FirstMVCApp.Controllers
+{
+    public class StudentController : Controller
+    {
+
+
+        public IActionResult Index()
+        {
+            ViewBag.title = "Student List";
+            ViewBag.Message = "Welcome to the student Module";
+            return View();
+        }
+
+        public IActionResult Details(int id)
+        {
+            ViewBag.StudentID = id;
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return Content("This is student module");
+        }
+
+    }
+}
