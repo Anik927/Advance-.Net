28b095eb542139b167dba47e873143890f37b1b8
Anik927 <mirzaanik927@gmail.com>
2026-07-11 12:57:17 +0600

    task 2

---
+++ Task_2/FirstMVCApp/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FirstMVCApp.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult List(string category, int id)
+        {
+            ViewBag.Category = category;
+            ViewBag.Id = id;
+            return View();
+        }
+
+    }
+}
---
+++ Task_2/FirstMVCApp/Controllers/StudentController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FirstMVCApp.Controllers
+{
+    public class StudentController : Controller
+    {
+
+
+        public IActionResult Index()
+        {
+            ViewBag.Title = "Student List"; 
+            ViewData["SubTitle"] = "Enrolled students this semester"; 
+            var students = new List<string> { "Alice Rahman", "Bob Hasan", "Carol Akter", "David Khan" }; 
+            return View(students);
+        }
+
+        public IActionResult Details(int id)
+        {
+            ViewBag.StudentID = id;
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return Content("This is student module");
+        }
+
+    }
+}
