f11e4252bd0c0cd03842ed1ad4fb0f982233b0d5
Anik927 <mirzaanik927@gmail.com>
2026-07-27 19:21:42 +0600

    Task-3

---
+++ Task_3/FirstMVCApp/Controllers/RegistrationController.cs
@@ -0,0 +1,38 @@
+﻿//using FirstMVCApp.Models;
+using FirstMVCApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstMVCApp.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(Student student)
+        {
+            // Check for duplicate student ID manually
+            if (student.StudentId == "STU001") 
+            {
+                ModelState.AddModelError("StudentId", "Student ID STU001 is already registered");
+            } 
+            if (!ModelState.IsValid) 
+            {
+                return View(student); 
+            } 
+            ViewBag.Message = "Registration successful for " + student.Name; 
+            return View("Success", student);
+
+        }
+
+    }
+}
---
+++ Task_3/FirstMVCApp/Models/Student.cs
@@ -0,0 +1,33 @@
+﻿using FirstMVCApp.Validations;
+using System.ComponentModel.DataAnnotations;
+
+namespace FirstMVCApp.Models
+{
+    public class Student
+    {
+        [Required(ErrorMessage = "ID is required")]
+        [StringLength(10, ErrorMessage = " ID can't be more than 10 char")]
+        public string StudentId { set; get;  }
+
+        [Required(ErrorMessage ="Name is required")]
+        [StringLength(100, MinimumLength =3,ErrorMessage ="Name must be between 3 and 100 char")]
+        public string Name {  set; get; }
+
+        [Required(ErrorMessage ="Email is required")]
+        [EmailAddress(ErrorMessage ="invalid email address")]
+        public string Email {  set; get; }
+
+        [Required(ErrorMessage ="Age is required")]
+        [Range(16,40, ErrorMessage ="Age must be between 16 and 40")]
+        public int Age {  set; get; }
+
+        [Required(ErrorMessage ="GPA is required")]
+        [Range(0.0,4.0,ErrorMessage ="Invalid CGPA")]
+        public double GPA { set; get; }
+
+        [Required(ErrorMessage = "Date of birth is required")]
+        [MinimumAge(16)] 
+        public DateTime DateOfBirth { get; set; }
+
+    }
+}
